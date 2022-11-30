a47243f5adce5b722a7a2a0dacfe19cb481cd246
Christoph Martel <christoph.martel@gmail.com>
2022-11-30 22:27:22 +0100

    clean up

--- code.py
+++ code.py
@@ -2,43 +2,41 @@
   Control DC motor using Maker Drive and CircuitPython on RP2040.
 """
 
-import time
+from adafruit_motor import motor
+import adafruit_ssd1306
 import board
+import busio as io
 import digitalio
 import pwmio
-from adafruit_motor import motor
 import rotaryio
-import busio as io
-import adafruit_ssd1306
+import time
 
 PWM_M1A = board.GP2  # pick any pwm pins on their own channels
 PWM_M1B = board.GP3
-PWM_M2A = board.GP4  # pick any pwm pins on their own channels
-PWM_M2B = board.GP5
 PWM_FREQ = 10000  # Custom PWM frequency
+SCL = board.GP15
+SDA = board.GP14
 DECAY_MODE = motor.SLOW_DECAY  # Set controller to Slow Decay (braking) mode
 THROTTLE_HOLD = 10  # Hold the throttle (seconds)
+MEASUREMENTS = 1
+MAX_MEASUREMENTS = 10
+LAST_EVAL_TIME = time.monotonic()
+
 
 # DC motor setup
 pwm_1a = pwmio.PWMOut(PWM_M1A, frequency=PWM_FREQ)
 pwm_1b = pwmio.PWMOut(PWM_M1B, frequency=PWM_FREQ)
 motor1 = motor.DCMotor(pwm_1a, pwm_1b)
-# pwm_2a = pwmio.PWMOut(PWM_M2A, frequency=PWM_FREQ)
-# pwm_2b = pwmio.PWMOut(PWM_M2B, frequency=PWM_FREQ)
-# motor2 = motor.DCMotor(pwm_2a, pwm_2b)
  
 # set up encoder
 encoder = rotaryio.IncrementalEncoder(board.GP16, board.GP17)
 motor1.throttle = 0.4
-LAST_EVAL_TIME = -1
 pos1 = -1
-start_cycle = True
+START_CYCLE = True
 rpm = 0
-measurements = 1
-max_measurements = 10
-LAST_EVAL_TIME = time.monotonic()
 
-i2c = io.I2C(board.GP15, board.GP14)
+# set up display
+i2c = io.I2C(SCL, SDA)
 oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
 
 def write_on_display(msg):
@@ -46,27 +44,31 @@ def write_on_display(msg):
   oled.text(msg, 0, 0, 1)
   oled.show()
 
-print(f"Polling motor speed in RPM every {THROTTLE_HOLD} seconds for {max_measurements} cycles.")
+def calc_rpm_from_ticks(pos1, pos2):
+  ticks = abs(pos2-pos1)
+  orig_rounds = ticks / 7
+  rounds = orig_rounds / 150
+  rpm = rounds * (60 / THROTTLE_HOLD)
+  return rpm
+
+print(f"Polling motor speed in RPM every {THROTTLE_HOLD} seconds for {MAX_MEASUREMENTS} cycles.")
 print(f"Motor is throttled at {motor1.throttle * 100}%.")
 
-while measurements <= max_measurements:
+while MEASUREMENTS <= MAX_MEASUREMENTS:
   now = time.monotonic()
-  if start_cycle:
+  if START_CYCLE:
     pos1 = encoder.position
-    start_cycle = False
+    START_CYCLE = False
   else:
     pos2 = encoder.position
     if now >= LAST_EVAL_TIME + THROTTLE_HOLD:
-      ticks = abs(pos2-pos1)
-      orig_rounds = ticks / 7
-      rounds = orig_rounds / 150
-      rpm = rounds * (60 / THROTTLE_HOLD)
-      start_cycle = True
+      rpm = calc_rpm_from_ticks(pos1, pos2)
+      START_CYCLE = True
       LAST_EVAL_TIME = now
-      msg = f'{measurements:02d} - Rounds per minute: {rpm:.1f}'
+      msg = f'{MEASUREMENTS:02d} - Rounds per minute: {rpm:.1f}'
       write_on_display(f'RPM: {rpm:.1f}')
       print(msg)
-      measurements = measurements + 1
+      MEASUREMENTS = MEASUREMENTS + 1
       
 motor1.throttle = 0  # Stop motor1
 print("Measurement completed.") 
